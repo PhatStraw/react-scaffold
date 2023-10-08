92944557784a22c7fc1222af4d782c2110440d22
PhatStraws <kevindsims1@gmail.com>
2023-10-07 22:40:12 -0700

    handle

--- postinstall.js
+++ postinstall.js
@@ -9,13 +9,26 @@ const projectPath = path.resolve(packagePath, '../../');
 // List of files/directories to copy
 const assetsToCopy = ['src', '.babelrc', 'webpack.config.js'];
 
+const packageJsonPath = path.join(projectPath, 'package.json');
+const packageJson = require(packageJsonPath);
+
+// Add webpack-dev-server as a dev dependency
+execSync('npm install webpack-dev-server --save-dev', { stdio: 'inherit' });
+
+// Add or modify the start script
+packageJson.scripts = packageJson.scripts || {};
+packageJson.scripts.start = "webpack serve --open --config webpack.config.js";
+
+// Write the updated package.json back to disk
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
 assetsToCopy.forEach((asset) => {
-  const sourcePath = path.join(packagePath, asset);
-  if (fs.existsSync(sourcePath)) {
-    fs.copySync(sourcePath, path.join(projectPath, asset));
-  } else {
-    console.warn(`Warning: ${asset} not found in react-phat package.`);
-  }
+    const sourcePath = path.join(packagePath, asset);
+    if (fs.existsSync(sourcePath)) {
+        fs.copySync(sourcePath, path.join(projectPath, asset));
+    } else {
+        console.warn(`Warning: ${asset} not found in react-phat package.`);
+    }
 });
 // Install peer dependencies
 execSync('npm install react react-dom', { stdio: 'inherit' });
