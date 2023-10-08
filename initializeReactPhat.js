010d11cf4e142f08adada565e8be2ddf3673644d
PhatStraws <kevindsims1@gmail.com>
2023-10-07 23:25:46 -0700

    handle

---
+++ initializeReactPhat.js
@@ -0,0 +1,37 @@
+// initializeReactPhat.js
+
+const fs = require('fs-extra');
+const path = require('path');
+const execSync = require('child_process').execSync;
+
+const packagePath = path.resolve(__dirname);
+const projectPath = process.cwd();  // This will now correctly point to the user's project root
+
+const assetsToCopy = ['src', '.babelrc', 'webpack.config.js'];
+
+const packageJsonPath = path.join(projectPath, 'package.json');
+const packageJson = require(packageJsonPath);
+
+// Add webpack, webpack-cli, and webpack-dev-server as dev dependencies
+execSync('npm install webpack webpack-cli webpack-dev-server --save-dev', { stdio: 'inherit' });
+
+// Modify the user's package.json start script
+packageJson.scripts = packageJson.scripts || {};
+packageJson.scripts.start = "webpack serve --open --config webpack.config.js";
+
+// Write the updated package.json back to disk
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+// Copy assets from react-phat to the user's project
+assetsToCopy.forEach((asset) => {
+    const sourcePath = path.join(packagePath, asset);
+    const destinationPath = path.join(projectPath, asset);
+    
+    if (fs.existsSync(sourcePath)) {
+        fs.copySync(sourcePath, destinationPath);
+    } else {
+        console.warn(`Warning: ${asset} not found in react-phat package.`);
+    }
+});
+
+console.log('react-phat initialization complete!');
--- postinstall.js
+++ postinstall.js
@@ -1,41 +1,6 @@
-const fs = require('fs-extra');
-const path = require('path');
-const execSync = require('child_process').execSync;
+console.log(`
+Thank you for installing react-phat!
 
-// Define paths
-const packagePath = path.resolve(__dirname);
-
-// Using npm_config_prefix to determine the project's root directory
-const projectPath = process.env.INIT_CWD;
-
-
-// List of files/directories to copy
-const assetsToCopy = ['src', '.babelrc', 'webpack.config.js'];
-
-const packageJsonPath = path.join(projectPath, 'package.json');
-const packageJson = require(packageJsonPath);
-
-// Add webpack, webpack-cli, and webpack-dev-server as dev dependencies
-execSync('npm install webpack webpack-cli webpack-dev-server --save-dev', { stdio: 'inherit', cwd: projectPath });
-
-// Add or modify the start script
-packageJson.scripts = packageJson.scripts || {};
-packageJson.scripts.start = "webpack serve --open --config webpack.config.js";
-
-// Write the updated package.json back to disk
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-
-assetsToCopy.forEach((asset) => {
-    const sourcePath = path.join(packagePath, asset);
-    const destinationPath = path.join(projectPath, asset);
-    
-    console.log(`Copying from ${sourcePath} to ${destinationPath}`);
-
-    if (sourcePath !== destinationPath && fs.existsSync(sourcePath)) {
-        fs.copySync(sourcePath, destinationPath);
-    } else if (sourcePath === destinationPath) {
-        console.warn(`Skipped copying ${asset} as source and destination are the same.`);
-    } else {
-        console.warn(`Warning: ${asset} not found in react-phat package.`);
-    }
-});
+To complete the setup, please run:
+npx initialize-react-phat
+`);
