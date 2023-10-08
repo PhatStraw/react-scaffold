650ed41f9ef15592321fa8ce0195ab6e9210199e
PhatStraws <kevindsims1@gmail.com>
2023-10-07 22:36:45 -0700

    handle

---
+++ postinstall.js
@@ -0,0 +1,21 @@
+const fs = require('fs-extra');
+const path = require('path');
+const execSync = require('child_process').execSync;
+
+// Define paths
+const packagePath = path.resolve(__dirname);
+const projectPath = path.resolve(packagePath, '../../');
+
+// List of files/directories to copy
+const assetsToCopy = ['src', '.babelrc', 'webpack.config.js'];
+
+assetsToCopy.forEach((asset) => {
+  const sourcePath = path.join(packagePath, asset);
+  if (fs.existsSync(sourcePath)) {
+    fs.copySync(sourcePath, path.join(projectPath, asset));
+  } else {
+    console.warn(`Warning: ${asset} not found in react-phat package.`);
+  }
+});
+// Install peer dependencies
+execSync('npm install react react-dom', { stdio: 'inherit' });
