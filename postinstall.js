03b84e1a652b87a7a4b67b82fa7bc5b0c5f92dc7
PhatStraws <kevindsims1@gmail.com>
2023-10-08 10:51:59 -0700

    postinstall

--- postinstall.js
+++ postinstall.js
@@ -9,6 +9,19 @@ const projectPath = path.resolve(packagePath, '../../');
 // List of files/directories to copy
 const assetsToCopy = ['src', '.babelrc', 'webpack.config.js'];
 
+const packageJsonPath = path.join(projectPath, 'package.json');
+const packageJson = require(packageJsonPath);
+
+// Add webpack, webpack-cli, and webpack-dev-server as dev dependencies
+execSync('npm install webpack webpack-cli webpack-dev-server --save-dev', { stdio: 'inherit' });
+
+// Add or modify the start script
+packageJson.scripts = packageJson.scripts || {};
+packageJson.scripts.start = "webpack serve --open --config webpack.config.js";
+
+// Write the updated package.json back to disk
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
 assetsToCopy.forEach((asset) => {
     const sourcePath = path.join(packagePath, asset);
     if (fs.existsSync(sourcePath)) {
@@ -18,5 +31,5 @@ assetsToCopy.forEach((asset) => {
     }
 });
 
-// Install peer dependencies and other necessary plugins/loaders
-execSync('npm install react react-dom html-webpack-plugin --save-dev', { stdio: 'inherit' });
+// Install peer dependencies
+execSync('npm install react react-dom', { stdio: 'inherit' });
